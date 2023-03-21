49a3539c73990801ebc396d582d0e5ddd4b43ac8
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-03-21 11:56:20 -0500

    merge with joe

--- src/main/java/com/codeup/adlister/dao/MySQLTagsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLTagsDao.java
@@ -1,4 +1,36 @@
 package com.codeup.adlister.dao;
 
-public class MySQLTagsDao {
+import com.codeup.adlister.models.Tag;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MySQLTagsDao implements Tags {
+
+    private Connection connection = null;
+
+    public MySQLTagsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+    @Override
+    public List<Tag> all() {
+        return null;
+    }
+
+    @Override
+    public Tag findByTagId(String id) {
+        return null;
+    }
 }
--- src/main/java/com/codeup/adlister/models/Tag.java
+++ src/main/java/com/codeup/adlister/models/Tag.java
@@ -10,7 +10,4 @@ import lombok.*;
 public class Tag {
     private String tag;
     private Long tagID;
-
-
-
 }
